f7ea57b71223de01d072ca06e5bc47fc77ae3a45
Maria de Araujo <mariahelenatw@gmail.com>
2024-07-21 16:58:24 -0300

    feat: creating text field component

---
+++ src/components/TextField/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TextField.css";
+const TextField = () => {
+  return (
+    <div className="text-field">
+      <label>Nome</label>
+      <input placeholder="Type in your name" />
+    </div>
+  );
+};
+
+export default TextField;
